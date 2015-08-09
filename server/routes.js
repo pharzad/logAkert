09fd54725c2c101454edb7a9148c4a85dd164a39
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-08-09 00:58:16 -0700

    latest

--- server/routes.js
+++ server/routes.js
@@ -4,16 +4,20 @@
 
 'use strict';
 
+var errors = require('./components/errors');
 var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Insert routes below
-//  app.use('/api/things', require('./api/thing'));
-  app.use('/api/logs', require('./api/log'));    
-  
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
-    });
+    // Insert routes below
+    //  app.use('/api/things', require('./api/thing'));
+    app.use('/api/logs', require('./api/log'));
+
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+        .get(errors[404]);
+    
+    app.route('/*')
+        .get(function (req, res) {
+            res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+        });
 };
