32a741184e3512473bd8d2f58c07382a970dfcaf
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-08-10 11:58:53 -0700

    change the look

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -39,6 +39,11 @@ angular.module('portOfAdvsApp')
         };
 
         httpServices.getLatestStatus().then(function (agentsStatus) {
+            
+            console.log($scope.agents.status.agentExtension);
+            
+            console.log(typeof $scope.agents.status.agentExtension);
+            
             $scope.agents.status = agentsStatus.data;
         });
 
