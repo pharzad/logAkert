c0a12aaac75e3285e951627afd2f3d31fd50a19d
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-08-12 11:11:53 -0700

    adding extension

--- server/routes.js
+++ server/routes.js
@@ -14,7 +14,7 @@ app.get('/extension/:name', function (req, res, next) {
 
     console.log('hello');
   var options = {
-    root: app.get('appPath') + '/extension/',
+    root: __dirname+ '/extension/',
     dotfiles: 'deny',
     headers: {
         'x-timestamp': Date.now(),
