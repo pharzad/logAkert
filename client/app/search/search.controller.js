5a7d4de54ed3d988f40d7d2c0056c8cfa2e6c77c
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-08-14 19:23:51 -0700

    latest

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -5,23 +5,39 @@ angular.module('portOfAdvsApp')
 
  $scope.goSearch = function(){
  
-     console.log($scope.search);
+     console.log($scope.search); 
      
-     for (var key in $scope.search) {
-   if ($scope.search.hasOwnProperty(key)) {
-       var obj = $scope.search[key];
-        for (var prop in obj) {
-          // important check that this is objects own property 
-          // not from prototype prop inherited
-          if(obj.hasOwnProperty(prop)){
-            console.log(prop + ' = '+ obj[prop]);
-          }
+     if (typeof $scope.search !== 'undefined')
+     {
+       if ($scope.search.date)
+       {
+           if ($scope.search.date.from && $scope.search.date.to)
+           {
+               $scope.search.$and=[ { timeStamp: { $gte: $scope.search.date.from } }, { timeStamp: { $lte: $scope.search.date.to } } ];
+           }
+           else if($scope.search.date.from){
+               $scope.search.timeStamp = {$gte: $scope.search.date.from};
+           }
+            else if($scope.search.date.to){
+               $scope.search.timeStamp = {$lte: $scope.search.date.to};
+           }
+           delete $scope.search.date;
        }
-    }
-}
-     
-     
- 
+         console.log($scope.search);
+     }
+//     for (var key in $scope.search) {
+//   if ($scope.search.hasOwnProperty(key)) {
+//       var obj = $scope.search[key];
+//       
+//        for (var prop in obj) {
+//          // important check that this is objects own property 
+//          // not from prototype prop inherited
+//          if(obj.hasOwnProperty(prop)){
+//            console.log(prop + ' = '+ obj[prop]);
+//          }
+//       }
+//    }
+//}
  };
 
         socket.socket.on('log:save', function (res) {
