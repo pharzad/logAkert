c3f29b7a198bb8efda99dace4f0b75cd55e4ef53
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-08-14 20:02:22 -0700

    latest

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,48 +1,51 @@
 'use strict';
 
 angular.module('portOfAdvsApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
+    .controller('NavbarCtrl', function ($scope, $location, Auth) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
+    }, {
+            'title': 'Search',
+            'link': '/search'
     }];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.isCollapsed = true;
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
+    });
 
 angular.module('portOfAdvsApp')
-  .controller('secondNavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Agents Status',
-      'link': '/'
-    },{
-      'title': 'Latest Logs',
-      'link': '/latestLogs'
+    .controller('secondNavbarCtrl', function ($scope, $location, Auth) {
+        $scope.menu = [{
+            'title': 'Agents Status',
+            'link': '/'
+    }, {
+            'title': 'Latest Logs',
+            'link': '/latestLogs'
     }];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.isCollapsed = true;
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
+    });
