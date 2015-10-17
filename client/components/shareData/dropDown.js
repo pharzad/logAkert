31933f374969bd2e091a7ee129f526f54772a3cb
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-10-16 19:15:56 -0700

    latest

--- client/components/shareData/dropDown.js
+++ client/components/shareData/dropDown.js
@@ -4,16 +4,14 @@ angular.module('portOfAdvsApp')
     .factory('dropDown', function (httpServices) {
 
         var dropDown = {};
-        if (dropDown === {}) {
-            httpServices.getDropDowns().then(function (drop) {
-                dropDown = drop
-            });
-        }
+        httpServices.getDropDowns().then(function (drop) {
+            dropDown = drop
+        });
         var object = {};
 
         object.getDropDowns = function () {
             return dropDown;
         };
-    
-    return object;
+
+        return object;
     });
