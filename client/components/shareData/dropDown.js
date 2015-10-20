90dc093752c9d8ab2300608444a6c612fdc12e31
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-10-20 16:44:54 -0700

    latest

--- client/components/shareData/dropDown.js
+++ client/components/shareData/dropDown.js
@@ -19,7 +19,7 @@ angular.module('portOfAdvsApp')
                 return dropDownLoader();
             } else {
                 def.resolve(dropDown);
-                return def.promise();
+                return def.promise;
             }
         };
     
