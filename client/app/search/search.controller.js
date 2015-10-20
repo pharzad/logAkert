b0fddfbd2839b8e01f446c18444727ebcb3cc046
Farzad Aziminia <farzad.aziminia@objectbrains.com>
2015-10-20 16:02:09 -0700

    latest

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -25,43 +25,41 @@ angular.module('portOfAdvsApp')
                 stringSeach = stringSeach + '"agent.name":"' + search.name + '",';
             if (search.number === null)
                 stringSeach = stringSeach + '"callInfo.number":"' + search.number + '",';
+            if (search.duration)
+                stringSeach = stringSeach + '"webSocket.duration" : { "$gte":' + search.duration + '}';
+            var tmpLast = stringSeach.charAt(stringSeach.length - 1);
+
             if (search.date) {
                 if ((search.date.from && search.date.to) && (search.date.from !== '' && search.date.to !== '')) {
-                    if (search.date.from && search.date.from !== '') {
-                        stringSeach = stringSeach + '"timeStamp" : { "$gte":"' + new Date(search.date.from).toUTCString() + '"}';
-                    }
-                    if (search.date.to && search.date.to !== '') {
-                        stringSeach = stringSeach + '"timeStamp" : { "$lte":"' + new Date(search.date.to).toUTCString() + '"}';
-                    }
+                    stringSeach = stringSeach + '"$and":[{"timeStamp" : { "$gte":"' + new Date(search.date.from).toUTCString() + '"}},{"timeStamp" : { "$lte":"' + new Date(search.date.from).toUTCString() + '"}}]';
+                } else if (search.date.from && search.date.from !== '') {
+                    stringSeach = stringSeach + '"timeStamp" : { "$gte":"' + new Date(search.date.from).toUTCString() + '"}';
+                } else if (search.date.to && search.date.to !== '') {
+                    stringSeach = stringSeach + '"timeStamp" : { "$lte":"' + new Date(search.date.to).toUTCString() + '"}';
                 }
             }
-            if (typeof $scope.search !== 'undefined') {
-                if (search.duration)
-                    stringSeach = stringSeach + '"webSocket.duration" : { "$gte":' + search.duration + '}';
-                var tmpLast = stringSeach.charAt(stringSeach.length - 1);
-                if (tmpLast === ',') {
-                    stringSeach = stringSeach.slice(0, stringSeach.length - 1);
-                }
-                stringSeach = stringSeach + '}';
-//                if (search.date) {
-//                    if ((search.date.from && search.date.to) && (search.date.from !== '' && search.date.to !== '')) {
-//                        stringSeach = '{"$and":[' + stringSeach + ',';
-//                        var dateQuery = '';
-//                        dateQuery = '"timeStamp": {"$gte":' + search.date.from + '}}, {"timeStamp": {"$lte": ' + search.date.to + '}';
-//                        stringSeach = stringSeach + dateQuery + ']}';
-//                    }
-//                }
-
-                console.log(stringSeach);
-                httpServices.search(stringSeach).then(function (res) {
-
-                    if (res.status === 201 || res.status === 200) {
-                        $scope.searchResult = res.data;
-                    }
-                    console.log(res);
-
-                });
+            if (tmpLast === ',') {
+                stringSeach = stringSeach.slice(0, stringSeach.length - 1);
             }
+            stringSeach = stringSeach + '}';
+            //                if (search.date) {
+            //                    if ((search.date.from && search.date.to) && (search.date.from !== '' && search.date.to !== '')) {
+            //                        stringSeach = '{"$and":[' + stringSeach + ',';
+            //                        var dateQuery = '';
+            //                        dateQuery = '"timeStamp": {"$gte":' + search.date.from + '}}, {"timeStamp": {"$lte": ' + search.date.to + '}';
+            //                        stringSeach = stringSeach + dateQuery + ']}';
+            //                    }
+            //                }
+
+            console.log(stringSeach);
+            httpServices.search(stringSeach).then(function (res) {
+
+                if (res.status === 201 || res.status === 200) {
+                    $scope.searchResult = res.data;
+                }
+                console.log(res);
+
+            });
             //            for (var key in $scope.search) {
             //                if ($scope.search.hasOwnProperty(key)) {
             //                    var obj = $scope.search[key];
