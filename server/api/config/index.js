374eb239fe676c270d2dca57f5dd7a8f741044f1
Farad Aziminia <farzad.aziminia@gmail.com>
2017-01-11 14:28:34 -0800

    callbut

--- server/api/config/index.js
+++ server/api/config/index.js
@@ -4,20 +4,21 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.json({
-        ping: {
-            status: false,
-            interval: 10000
-        },
-        forceOffile: {
-            refresh: true,
-            force: true
-        },
-        pingTime: 1000,
-        planBInterval: 60000,
-        CallButt: true
-    });
+router.get('/', function(req, res) {
+  res.json({
+    ping: {
+      status: false,
+      interval: 10000
+    },
+    forceOffile: {
+      refresh: true,
+      force: true
+    },
+    pingTime: 1000,
+    planBInterval: 60000,
+    CallButt: true,
+    stampBut: true
+  });
 });
 
 module.exports = router;
