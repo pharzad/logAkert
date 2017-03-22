ef09aebe24de6a7f011e322a4bfda21318547271
Farad Aziminia <farzad.aziminia@gmail.com>
2017-03-22 10:57:42 -0700

    conflict

--- server/api/conflict/conflict.controller.js
+++ server/api/conflict/conflict.controller.js
@@ -8,15 +8,15 @@ exports.findConflict = function(payload) {
     'agent.extention': payload.agent.extention
   }).sort({
     timeStamp: -1
-  }).limit(1).exec(function(err, result) {
+  }).limit(2).exec(function(err, result) {
     if (err)
       return false;
 
       console.log(payload.timeStamp);
-      console.log(result[0].timeStamp);
+      console.log(result[1].timeStamp);
       console.log(payload.agent.extention);
 
-    if (result[0].agent.ip !== payload.agent.ip)
+    if (result[1].agent.ip !== payload.agent.ip)
       console.log('FUCKKKKKKKKK');
 
     else {
