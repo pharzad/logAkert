308fa0434a813ecb4d05ff79fc0b8b35c388a686
Farad Aziminia <farzad.aziminia@gmail.com>
2017-03-23 14:46:07 -0700

    conflicts

--- client/app/conflicts/conflicts.controller.js
+++ client/app/conflicts/conflicts.controller.js
@@ -3,6 +3,8 @@
 angular.module('portOfAdvsApp')
   .controller('ConflictsCtrl', function($scope, httpServices) {
     httpServices.getConflicts().then(function(result) {
-      console.log(result);
+      if (result.status === 200)
+
+      $scope.agents = result.data;
     })
   });
