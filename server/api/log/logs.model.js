f028bc53384a7ac33c551357796a5c1ee6bf92d2
Farad Aziminia <farzad.aziminia@gmail.com>
2017-04-13 13:19:21 -0700

    cpu logs

--- server/api/log/logs.model.js
+++ server/api/log/logs.model.js
@@ -1,48 +1,60 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 var LogSchema = new Schema({
-    agent: {
-        name: String,
-        ip: String,
-        extention: Number,
-        version: String
+  agent: {
+    name: String,
+    ip: String,
+    extention: Number,
+    version: String
+  },
+  callInfo: {
+    Calldirection: String,
+    number: Number,
+    duration: Number,
+    callType: String
+  },
+  error: {
+    errorType: String,
+    errorMessage: String,
+    line: String
+  },
+  webSocket: {
+    webSocketFunction: String,
+    webSocketBody: String,
+    duration: Number,
+    confirmation: String,
+  },
+  freeSwitch: {
+    duration: Number,
+    code: String,
+    reason: String
+  },
+  status: {
+    newStatus: String,
+    statusDuration: Number,
+    previewsStatus: String,
+    triggered: String
+  },
+  system: {
+    cpu: {
+      idle: Number,
+      modelName: String,
+      numOfProcessors: Number,
+      total: Number
     },
-    callInfo: {
-        Calldirection: String,
-        number: Number,
-        duration: Number,
-        callType: String
-    },
-    error: {
-        errorType: String,
-        errorMessage: String,
-        line: String
-    },
-    webSocket: {
-        webSocketFunction: String,
-        webSocketBody: String,
-        duration: Number,
-        confirmation: String,
-    },
-    freeSwitch: {
-        duration: Number,
-        code: String,
-        reason: String
-    },
-    status: {
-        newStatus: String,
-        statusDuration: Number,
-        previewsStatus: String,
-        triggered: String
-    },
-    logType: String,
-    uuId: String,
-    freeSwitchAddress: String,
-    timeStamp: Date,
-    active: Boolean
+    memory: {
+      availableCapacity: Number,
+      capacity: Number
+    }
+  },
+  logType: String,
+  uuId: String,
+  freeSwitchAddress: String,
+  timeStamp: Date,
+  active: Boolean
 });
 
 module.exports = mongoose.model('Log', LogSchema);
